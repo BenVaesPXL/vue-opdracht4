19a1b32191799c2315fd69d429592672197075e2
BennieV15 <12100455@student.pxl.be>
2025-03-12 12:29:55 +0100

    opdracht 4

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,25 @@
 <script>
-
+  export default {
+    data(){
+      return {
+        title: "Web Advanced",
+        intro: "Welkom in deze cursus",
+        linkUrl: "https://vuejs.org",
+        imageUrl:"https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/1200px-Vue.js_Logo_2.svg.png",
+        imageheight: 100,
+        imagewidth: 100
+      }
+    }
+  }
 </script>
 
 <template>
-  <h1>Welkom in de Vue App</h1>
+  <h1>{{title}}</h1>
+  <p>{{intro}}</p>
+  <a :href="linkUrl">
+    <img :src="imageUrl" alt="vue logo" :height="imageheight" :width="imagewidth" />
+  </a>
 </template>
 
-<style>
+<style scoped>
 </style>
